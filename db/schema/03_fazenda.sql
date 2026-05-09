20c55ea6528d4f327bbfe4f7cb5d900a4459dcc3
guibbsss <39602800+guibbsss@users.noreply.github.com>
2026-05-09 14:51:49 -0300

    correção

--- db/schema/03_fazenda.sql
+++ db/schema/03_fazenda.sql
@@ -8,3 +8,15 @@ CREATE TABLE fazenda (
   proprietario VARCHAR(200) NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+ALTER TABLE `safracerta`.`fazenda` 
+CHANGE COLUMN `proprietario` `proprietario` BIGINT NULL DEFAULT NULL ,
+ADD INDEX `fk_proprietario_user_idx` (`proprietario` ASC) VISIBLE;
+;
+ALTER TABLE `safracerta`.`fazenda` 
+ADD CONSTRAINT `fk_proprietario_user`
+  FOREIGN KEY (`proprietario`)
+  REFERENCES `safracerta`.`usuario` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
